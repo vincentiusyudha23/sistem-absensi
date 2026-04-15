28456878319e0b38209b79e764862edc6607fd42
Vincentius Yudha R <92367937+118140170Vincentius@users.noreply.github.com>
2026-04-15 12:48:43 +0700

    fix map

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -130,7 +130,7 @@
                         </button>
                     </div>
                     <div class="col-lg-8 col-12">
-                        <div class="rounded" id="map" style="height: 100%;"></div>
+                        <div class="rounded" id="map"></div>
                     </div>
                     <input name="latitude" type="hidden" id="latitude">
                     <input name="longitude" type="hidden" id="longitude">
